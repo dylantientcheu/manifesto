86fc03f37affdf9105b693c452dac2e5437aabff
Dylan Tientcheu <dylantientcheu@gmail.com>
2022-07-24 12:31:08 +0200

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,20 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
-import ViteComponents from 'vite-plugin-components'
+import Components from 'unplugin-vue-components/vite'
 import WindiCSS from 'vite-plugin-windicss'
 import { VitePWA } from 'vite-plugin-pwa'
-import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
   base: '/manifesto/',
   plugins: [
     Vue(),
-    ViteComponents({
-      globalComponentsDeclaration: true,
-      customComponentResolvers: [
-        ViteIconsResolver({
-          componentPrefix: '',
-        }),
-      ],
-    }),
+    Components(),
     WindiCSS({
-      safelist: 'prose prose-sm m-auto text-left',
+      safelist: 'prose prose-sm m-auto text-left'
     }),
-    ViteIcons(),
+    Icons({ autoInstall: true }),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
@@ -34,30 +27,29 @@ export default defineConfig({
           {
             src: './img/icons/favicon-32x32.png',
             sizes: '32x32',
-            type: 'image/png',
+            type: 'image/png'
           },
           {
             src: './img/icons/android-chrome-512x512.png',
             sizes: '512x512',
-            type: 'image/png',
-          },
+            type: 'image/png'
+          }
         ],
         start_url: './index.html',
         display: 'standalone',
         background_color: '#ffffff',
-        theme_color: '#4DBA87',
-      },
-    }),
-
+        theme_color: '#4DBA87'
+      }
+    })
   ],
   resolve: {
     alias: [
       {
         find: '~',
-        replacement: path.resolve(__dirname, 'src'),
-      },
-    ],
-  },
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ]
+  }
   /* optimizeDeps: {
     include: ['vue', 'vue-router', '@vueuse/core'],
   }, */
